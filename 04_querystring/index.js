669838a41aab7d9810663750520d0e180f5d81fb
Andrii Zilnyk <andrii.zilnyk@gmil.com>
2020-12-22 15:05:10 +0200

    changes

--- 04_querystring/index.js
+++ 04_querystring/index.js
@@ -3,6 +3,24 @@ const querystring = require('querystring');
 
 const parsedUrl = url.parse('http://www.google.com/?page=3&limit=10&category=nodejs&category=javascript');
 
+console.log(parsedUrl);
+/*
+* {
+      protocol: 'http:',
+      slashes: true,
+      auth: null,
+      host: 'www.google.com',
+      port: null,
+      hostname: 'www.google.com',
+      hash: null,
+      search: '?page=3&limit=10&category=nodejs&category=javascript',
+      query: 'page=3&limit=10&category=nodejs&category=javascript',
+      pathname: '/',
+      path: '/?page=3&limit=10&category=nodejs&category=javascript',
+      href: 'http://www.google.com/?page=3&limit=10&category=nodejs&category=javascript'
+  }
+* */
+
 const query = querystring.parse(parsedUrl.query);
 
 console.log(query); // { page: '3', limit: '10', category: [ 'nodejs', 'javascript' ] }
