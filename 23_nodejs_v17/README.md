015da85f6bdf6f3709e5bd4cf0acccf2d0af666d
Andrii Zilnyk <andrii.zilnyk@gmail.com>
2022-01-30 16:08:15 +0200

    added new sections

---
+++ 23_nodejs_v17/README.md
@@ -0,0 +1,8 @@
+# NodeJS v17
+* OpenSSL 3.0 support
+* V8 JavaScript engine is updated to V8 9.5
+* Readline Promise API
+
+```
+    Now using node v17.4.0 (npm v8.3.1)
+```
---
+++ 23_nodejs_v17/index.js
@@ -0,0 +1,22 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'process';
+
+async function start() {
+  try {
+    const rl = readline.createInterface({ input, output });
+
+    const name = await rl.question('What is your name? ');
+    console.log(`Hello, ${name}!`);
+
+    const answer = await rl.question('What do you think of Node 17? ');
+    console.log(`Cool, ${name} thinks it is ${answer}!`);
+
+    rl.close();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+start()
+  .then(console.log)
+  .catch(console.error);
